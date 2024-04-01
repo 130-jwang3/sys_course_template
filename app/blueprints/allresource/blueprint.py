36be97e94c1ba5f3ed9f7ec8a29a505f0760b46d
Christopher Tan <02c.albert@gmail.com>
2024-03-31 22:54:28 -0700

    update all reesources and courses to use api gateway

--- app/blueprints/allresource/blueprint.py
+++ app/blueprints/allresource/blueprint.py
@@ -1,9 +1,11 @@
-from helpers import courses, resources
+from helpers import auth
+import os
 from middlewares.auth import auth_required, auth_optional
 
 from flask import Blueprint, render_template
 
 all_resource_page = Blueprint('all_resource_page', __name__)
+API_GATEWAY = os.environ.get('API_GATEWAY_URL')
 
 @all_resource_page.route('/all_resource')
 @auth_optional
@@ -11,10 +13,21 @@ def display(auth_context):
     """
     View function for displaying the resources page.
     """
-    resource_items = resources.list_resources()
+    api_gateway_url = API_GATEWAY + "/resources"
+    jwt_cred = auth.generate_creds(
+        sa_keyfile="keyfile.json",
+        sa_email=os.environ.get('JWT_EMAIL'),
+        audience=API_GATEWAY
+    )
+    response = auth.make_authorized_get_request(
+        jwt_cred,
+        url=api_gateway_url
+    )
+    resource_items = response.json()
+    
     return render_template(
         "all_resource_page.html",
         resources=resource_items,
         auth_context=auth_context,
-        bucket=courses.BUCKET,
+        bucket=os.environ.get('GCS_BUCKET'),
     )
--- app/blueprints/get_course/blueprint.py
+++ app/blueprints/get_course/blueprint.py
@@ -1,9 +1,11 @@
-from helpers import courses, resources
+import os
+from helpers import auth
 from middlewares.auth import auth_required, auth_optional
 
 from flask import Blueprint, render_template
 
 all_course_page = Blueprint('all_course_page', __name__)
+API_GATEWAY = os.environ.get('API_GATEWAY_URL')
 
 @all_course_page.route('/all_course')
 @auth_optional
@@ -11,7 +13,18 @@ def display(auth_context):
     """
     View function for displaying the courses page.
     """
-    course_items = courses.list_course()
+    api_gateway_url = API_GATEWAY + "/courses"
+    jwt_cred = auth.generate_creds(
+        sa_keyfile="keyfile.json",
+        sa_email=os.environ.get('JWT_EMAIL'),
+        audience=API_GATEWAY
+    )
+    response = auth.make_authorized_get_request(
+        jwt_cred,
+        url=api_gateway_url
+    )
+    course_items = response.json()
+    
     return render_template(
         "all_course_page.html",
         courses=course_items,
